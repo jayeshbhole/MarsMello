5851145e43b764590cd0a956db6175b24cb41d6b
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2021-07-15 20:12:14 +0530

    Truffle Bamboozle

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 // const infuraKey = "fj4jll3k.....";
 //
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const fs = require("fs");
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
 	/**
@@ -42,11 +42,18 @@ module.exports = {
 		// tab if you use this network and you must also set the `host`, `port` and `network_id`
 		// options below to some value.
 		//
-		// development: {
-		//  host: "127.0.0.1",     // Localhost (default: none)
-		//  port: 8545,            // Standard Ethereum port (default: none)
-		//  network_id: "*",       // Any network (default: none)
-		// },
+		development: {
+			host: "127.0.0.1", // Localhost (default: none)
+			port: 8545, // Standard Ethereum port (default: none)
+			network_id: "*", // Any network (default: none)
+		},
+		matic: {
+			provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.matic.today`),
+			network_id: 80001,
+			confirmations: 2,
+			timeoutBlocks: 200,
+			skipDryRun: true,
+		},
 		// Another network with more advanced options...
 		// advanced: {
 		// port: 8777,             // Custom port
