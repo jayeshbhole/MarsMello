44912aa6aec917758dfce15ef6aa3f3a29c4a268
Harsh Prakash <prakashharsh32@gmail.com>
2021-07-15 20:27:35 +0530

    erc20

--- contracts/Marsmello.sol
+++ contracts/Marsmello.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.8.0;
 
-contract MarsMello {}
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+contract MarsMello is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
+        _mint(msg.sender, initialSupply);
+    }
+}
