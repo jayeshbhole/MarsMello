b09dd7dca4c4cd29a8f68ee0b744e8eaa64a1016
Harsh Prakash <37194228+harsh132@users.noreply.github.com>
2021-07-23 17:30:11 +0530

    Update Marsmello.sol

--- contracts/Marsmello.sol
+++ contracts/Marsmello.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts@4.2.0/token/ERC1155/ERC1155.sol";
@@ -28,3 +29,27 @@ contract MarsMellow is ERC1155,ERC20, Ownable, ERC1155Burnable {
     {
         _mint(account, id, amount, data);
     }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+    
+    function newLand(int x,int y) public {
+        uint test =0;
+        if(x<0){
+            test+=1;
+            x*=-1;
+        }
+        if(y<0){
+            test+=2;
+            y*=-1;
+        }
+        uint deploy_from = ((block.timestamp-deploy_time) / 86400) + 50;
+        require(uint(x)<=deploy_from && uint(y)<=deploy_from && deploy_from<1000000000,"Co-Ordinates out of bound !");
+        uint id=test*10**20+uint(x)*10**10+uint(y);
+        _mint(msg.sender,id,1,"");
+    }
+}
