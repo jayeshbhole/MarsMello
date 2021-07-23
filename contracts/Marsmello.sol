27cfc6fba9caafb78f9bb765d22617a857611681
Harsh Prakash <37194228+harsh132@users.noreply.github.com>
2021-07-23 17:29:38 +0530

    Update Marsmello.sol

--- contracts/Marsmello.sol
+++ contracts/Marsmello.sol
@@ -1,10 +1,30 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.2;
 
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.2.0/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts@4.2.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.2.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.2.0/token/ERC1155/extensions/ERC1155Burnable.sol";
 
-contract MarsMello is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
-        _mint(msg.sender, initialSupply);
+contract MarsMellow is ERC1155,ERC20, Ownable, ERC1155Burnable {
+    
+    string m_name = "MarsMellow";
+    string m_symbol = "MLO";
+    uint m_supply = 1000000000;
+    uint deploy_time;
+    
+    constructor() ERC1155("https://marsmello.io/uri/mello.json") ERC20(m_name, m_symbol) {
+        _mint(msg.sender, m_supply * 10 ** decimals());
+        deploy_time = block.timestamp;
     }
-}
+    
+    
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mint(account, id, amount, data);
+    }
