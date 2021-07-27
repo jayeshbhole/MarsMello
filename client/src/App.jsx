d8651b7dc39d5e80f70361c3aacf4510f9d22022
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2021-07-28 02:29:11 +0530

    Calculate Centre

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./styles/index.scss";
 import { useDrag } from "react-use-gesture";
 import { useSpring, animated } from "@react-spring/web";
 
 function App() {
+	const [hk, setHk] = useState([15, 15]);
 	const [plots, setPlots] = useState(
-		//[][]
-		Array.from({ length: 31 }, (v, ind) => {
-			if (ind < 5 || ind >= 26) return Array.from({ length: 31 }, () => 2);
-			return Array.from({ length: 31 }, (_, i) => {
-				if (i < 5 || i >= 26) return 2;
-				return 1;
+		Array.from({ length: 31 }, (_, y) => {
+			if (y < 5 || y >= 26) return Array.from({ length: 31 }, () => [1]);
+			return Array.from({ length: 31 }, (_, x) => {
+				if (x < 5 || x >= 26) return [1];
+				return [x - hk[0], -y + hk[1]];
 			});
 		})
 	);
@@ -37,10 +37,11 @@ function App() {
 				left: mx,
 			});
 			if (last) {
+				calculateCentre(top.goal, left.goal);
 				// console.log(top.goal, left.goal);
 				// Check if out of bounds
 				// fetch new data
-				fetchData(top.goal, left.goal);
+				// fetchData(top.goal, left.goal);
 				// Set data in the state
 				// setPlots()
 				// Set offset to overflow delta
@@ -50,10 +51,22 @@ function App() {
 		{ initial: () => [left.get(), top.get()] }
 	);
 
-	// const calculateCentre()
+	const calculateCentre = (top, left, callback) => {
+		const centre = [
+			Math.floor((-left - paddingSize - cellSize + windowWidth / 2) / cellSize) - 9,
+			-Math.floor((-top - paddingSize - cellSize + windowHeight / 2) / cellSize) + 9,
+		];
+		setCentre(centre);
+	};
 
-	const fetchData = (topGoal, leftGoal) => {
-		// topGoal, leftGoal
+	useEffect(() => {
+		console.log(...centre);
+	}, [centre]);
+	const fetchData = async ([x, y]) => {
+		// setHk(centre[0])
+		// setTimeout(() => {
+		// 	console.log("Coords", x, y);
+		// }, 2000);
 	};
 	const handleClick = () => {
 		if (top.idle && left.idle) alert("Hello");
@@ -71,19 +84,20 @@ function App() {
 										<div
 											key={index}
 											onClick={() => handleClick(index)}
-											className={`cell ${cell === 1 ? "plot" : "cloud"} ${
+											className={`cell ${cell.length !== 1 ? "plot" : "cloud"} ${
 												Math.round(Math.random()) ? "bright" : null
 											}`}
 											style={{ width: cellSize, height: cellSize }}>
-											{cell === 2 ? (
-												<img draggable="false" src="./assets/img/cloud.png" alt="" />
-											) : (
+											{cell.length !== 1 ? (
 												<img
 													draggable="false"
-													src={`./assets/img/pixplot_${Math.round(Math.random() * 4)}.png`}
+													src={`./assets/img/pixplot_${Math.floor(Math.random() * 1)}.png`}
 													alt="plot.png"
 												/>
+											) : (
+												<img draggable="false" src="./assets/img/cloud.png" alt="" />
 											)}
+											<h1>{cell.toString()}</h1>
 										</div>
 									);
 								})}
@@ -92,6 +106,16 @@ function App() {
 					})}
 				</animated.div>
 			</animated.div>
+			<span
+				style={{
+					position: "fixed",
+					top: "50%",
+					left: "50%",
+					transform: "translate(-50%,-50%)",
+					background: "red",
+				}}>
+				H
+			</span>
 		</div>
 	);
 }
