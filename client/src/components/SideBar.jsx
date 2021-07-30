dd520848d507599ad9ffad09601390f9d4eef067
Pushkar1809 <pushkarborkar1809@gmail.com>
2021-07-30 17:08:27 +0530

    Added skeleton for the bars

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import Profile from "../assets/profile.png";
+import Factory from "../assets/factory.png";
+import Miner from "../assets/miner.png";
+import Plots from "../assets/plots.png";
 
-const SideBar = () => {};
+const SideBar = () => {
+	<div className="sidebar">
+		<button>
+			<img src={Profile} alt="Profile Button" />
+		</button>
+		<button>
+			<img src={Factory} alt="Factory Button" />
+		</button>
+		<button>
+			<img src={Miner} alt="Miner Button" />
+		</button>
+		<button>
+			<img src={Plots} alt="Plots Button" />
+		</button>
+	</div>;
+};
 
 export default SideBar;
