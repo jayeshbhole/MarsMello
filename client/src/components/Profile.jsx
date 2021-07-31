9f501722fdf87fb25e22a02133efaf029086469e
Pushkar Borkar <pushkarborkar1809@gmail.com>
2021-07-31 23:43:08 +0530

    updated the profaile modal.

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -6,18 +6,17 @@ const Profile = () => {
 		<div className="profile">
 			<div className="picture">
 				<img src="./assets/profile.png" alt="" />
+				<p>Change avatar</p>
 			</div>
 			<div className="data">
 				<h1>
-					<span>Wallet Address </span> 0xBf61E1a7F698b2E68cB
-					<br />
-					c6BE51E9784Ce7ccc0B1F
+					<span>Wallet Address </span> 0xBf61E1a7F698b2E68cBc6BE51E9784Ce7ccc0B1F
 				</h1>
 				<h1>
 					<span>Wallet Provider</span>Portis
 				</h1>
-				<button>Change Address</button>
 			</div>
+			<button>Change Address</button>
 		</div>
 	);
 };
