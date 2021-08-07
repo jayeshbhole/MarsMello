890133e5c0b0c21b1751c473be339ea14623ebf5
Pushkar Borkar <pushkarborkar1809@gmail.com>
2021-08-07 19:10:53 +0530

    add factory button disabled logic added

--- client/src/components/Auxillary/ImgButton.jsx
+++ client/src/components/Auxillary/ImgButton.jsx
@@ -4,7 +4,14 @@ import "../../styles/imgbutton.scss";
 const ImgButton = ({ id, className, src, alt, onClick, dataname, disabled }) => {
 	return (
 		<button className={`img-btn hover-info ${className}`} data-name={dataname} disabled={disabled}>
-			<img id={id} className={className} src={src} alt={alt} onClick={onClick} draggable="false" />
+			<img
+				id={id}
+				className={className}
+				src={src}
+				alt={alt}
+				onClick={!disabled && onClick}
+				draggable="false"
+			/>
 		</button>
 	);
 };
--- client/src/components/MiniModals/Info.jsx
+++ client/src/components/MiniModals/Info.jsx
@@ -3,10 +3,10 @@ import { GameContext } from "../../context/GameContext";
 
 const Info = () => {
 	const { selectedBlock } = useContext(GameContext);
-	return selectedBlock ? (
+	return selectedBlock?.seed !== -1 ? (
 		<div className="info">
-			<span>This Land is owned by {/*userID*/}</span>
-			<span>This Land has {/*factoryCount*/} Factories.</span>
+			<span>Cords. of Land are {selectedBlock?.id}</span>
+			<span>This Land is owned by {selectedBlock?.owner.id}</span>
 		</div>
 	) : (
 		<div className="info">
