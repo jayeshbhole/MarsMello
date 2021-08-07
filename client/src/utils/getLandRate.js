34fda8af39e16c61161c66e4f82d1e7f8bf8d0d0
Harsh Prakash <prakashharsh32@gmail.com>
2021-08-08 02:26:59 +0530

    better land rate

--- client/src/utils/getLandRate.js
+++ client/src/utils/getLandRate.js
@@ -1,4 +1,12 @@
 const getLandrate = (seed, type) => {
+	let rate;
+	const base = {
+		0: 100,
+		1: 50,
+		2: 20,
+		3: 10,
+		4: 5
+	};
 	const land_dependent = {
 		0: true,
 		1: true,
@@ -6,6 +14,9 @@ const getLandrate = (seed, type) => {
 		3: true,
 		4: true
 	};
-	if (land_dependent[type]) return 50 + ((seed / 10 ** (ftype % 35)) % 51);
-	else return 100;
+	if (land_dependent[type]) rate = 50 + ((seed / 10 ** (type % 35)) % 51);
+	else rate = 100;
+	return parseInt((base[type] * rate) / 100);
 };
+
+export default getLandrate;
