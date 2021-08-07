1c1f7dcd4edf5b161d66269be5ef89621bac6f0b
Pushkar Borkar <pushkarborkar1809@gmail.com>
2021-08-07 21:49:17 +0530

    mini menu mini modals skeleton set up

--- client/src/components/MiniModals/Add.jsx
+++ client/src/components/MiniModals/Add.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GameContext } from "../../context/GameContext";
 
 const Add = () => {
-	return <div></div>;
+	const { selectedBlock } = useContext(GameContext);
+	return selectedBlock?.factory ? (
+		<div className="add-content">
+			<span className="prompt">
+				Do you want to place a factory at
+				<span className="cords">{`(${selectedBlock?.x}, ${selectedBlock?.y})`}</span>
+			</span>
+			<span className="select">
+				<select name="factory" id="factory-list">
+					<option value="fact01">Test</option>
+				</select>
+			</span>
+			<button className="place-factory">Place</button>
+		</div>
+	) : (
+		<div className="add-content">
+			<span className="prompt">No Factories in Inventory to place.</span>
+			<button>Buy a factory</button>
+		</div>
+	);
 };
 
 export default Add;
