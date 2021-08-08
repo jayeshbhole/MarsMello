c7d432dd7e98fb8e0127d682c57d903d54fe0418
Harsh Prakash <prakashharsh32@gmail.com>
2021-08-08 17:02:24 +0530

    display balances in decimals

--- client/src/utils/manageNumbers.js
+++ client/src/utils/manageNumbers.js
@@ -2,14 +2,14 @@ const manageNumbers = (n) => {
 	if (!n) return 0;
 	const decimals = 18;
 	n /= 10 ** decimals;
-	if (Math.floor(n / 1000000000) > 0) {
-		return `${Math.floor(n / 1000000000)}B`;
-	} else if (Math.floor(n / 1000000) > 0) {
-		return `${Math.floor(n / 1000000)}M`;
-	} else if (Math.floor(n / 1000) > 0) {
-		return `${Math.floor(n / 1000)}K`;
+	if (n / 1000000000 >= 1) {
+		return `${(n / 1000000000).toFixed(1)}B`;
+	} else if (n / 1000000 >= 1) {
+		return `${(n / 1000000).toFixed(1)}M`;
+	} else if (n / 1000 >= 1) {
+		return `${(n / 1000).toFixed(1)}K`;
 	} else {
-		return `${n}`;
+		return n.toString();
 	}
 };
 
