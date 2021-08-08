2f4028c71bb495a919cdaed3bfd1d7ff1772d8ab
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2021-08-08 19:00:48 +0530

    Info Tab Information

--- client/src/components/MiniModals/Info.jsx
+++ client/src/components/MiniModals/Info.jsx
@@ -1,28 +1,49 @@
 import React, { useContext } from "react";
 import { GameContext } from "../../context/GameContext";
 import { Web3Context } from "../../context/Web3Context";
-
+import getLandrate from "../../utils/getLandRate";
+import LandPreview from "../Auxillary/LandPreview";
+import resourceList from "../../utils/resourceList.json";
 const Info = () => {
 	const { selectedBlock } = useContext(GameContext);
 	const { account } = useContext(Web3Context);
-
+	console.log(resourceList);
 	return selectedBlock?.seed !== -1 ? (
 		<div className="info-content content">
-			<span className="cords">
-				Co-ordinates of this Land are
-				<span className="high">{` (${selectedBlock?.x}, ${selectedBlock?.x})`}</span>
-			</span>
-			<span className="prompt">
-				This Land is owned by
-				<span className="prompt-big">
-					{selectedBlock?.owner?.id !== account
-						? `${selectedBlock?.owner?.id?.slice(0, 6)}...`
-						: " You"}
+			<div className="preview-container">
+				<LandPreview cellData={selectedBlock} />
+			</div>
+			<div className="info">
+				Owner{" "}
+				{selectedBlock?.owner?.id !== account
+					? `${selectedBlock?.owner?.id?.slice(0, 6)}...`
+					: " You"}
+				<span className="cords">
+					<span className="high">{` (${selectedBlock?.x}, ${selectedBlock?.y})`}</span>
 				</span>
-			</span>
-			<span className="prompt">
-				Land has <span className="prompt-big">{selectedBlock?.factory}</span>
-			</span>
+				<span className="cords">Factory:</span>
+				<div className="ores">
+					{Object.keys(resourceList).map((key, _) => {
+						return (
+							<div key={key}>
+								<span className="label">{resourceList[key].name}</span>{" "}
+								{getLandrate(selectedBlock.seed, key)}
+							</div>
+						);
+					})}
+				</div>
+				{selectedBlock.factory ? (
+					<>
+						<div className="factory">
+							<div className="name">{selectedBlock.factory.name}</div>
+						</div>
+						<div className="produce">
+							<div className="-name">{resourceList[selectedBlock.factory.type].name}</div>
+							{getLandrate(selectedBlock.seed, selectedBlock.factory.type)} /hr
+						</div>
+					</>
+				) : null}
+			</div>
 		</div>
 	) : (
 		<div className="info-content content">
--- client/src/utils/getLandRate.js
+++ client/src/utils/getLandRate.js
@@ -5,14 +5,14 @@ const getLandrate = (seed, type) => {
 		1: 50,
 		2: 20,
 		3: 10,
-		4: 5
+		4: 5,
 	};
 	const land_dependent = {
 		0: true,
 		1: true,
 		2: true,
 		3: true,
-		4: true
+		4: true,
 	};
 	if (land_dependent[type]) rate = 50 + ((seed / 10 ** (type % 35)) % 51);
 	else rate = 100;
